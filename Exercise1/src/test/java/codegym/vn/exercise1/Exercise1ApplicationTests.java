dce3159cd1ff1e6af5790f0c81e59a2ad2614c61
VanDuong279 <vanduong27904@gmail.com>
2024-06-03 19:20:02 +0700

    Nop

---
+++ Exercise1/src/test/java/codegym/vn/exercise1/Exercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package codegym.vn.exercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
